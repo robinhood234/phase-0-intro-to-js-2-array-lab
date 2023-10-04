7db31462ee14af8084eda1d14723be507ec03d3b
robin nganga smith <saintofcookies.2021@gmail.com>
2023-10-04 20:33:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+  const cats =['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(name) {
+  cats.push('Ralph');
+};
+function destructivelyPrependCat(name) {
+  cats.unshift('Bob');
+};
+function destructivelyRemoveLastCat() {
+  cats.pop();
+};
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+};
+function appendCat(name) {
+  const catName = [...cats, 'Broom'];
+  return catName;
+};
+function prependCat(name) {
+  const catRemoved = ['Arnold', ...cats];
+  return catRemoved;
+};
+function removeLastCat() {
+const dltLsCat = [...cats];
+dltLsCat.pop();
+return dltLsCat;
+};
+
+function  removeFirstCat() {
+const fsCatDlt = [...cats];
+fsCatDlt.shift();
+return fsCatDlt;
+};
